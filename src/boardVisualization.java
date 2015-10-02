a2c1e0f2831595ab773e3d6d1a24e0563c08ef3b
Henrik Martinius Negård Mørk <mn.henrik@gmail.com>
2015-10-02 15:58:34 +0200

    Try to fix visualization.

--- src/boardVisualization.java
+++ src/boardVisualization.java
@@ -3,8 +3,12 @@
  */
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -15,31 +19,57 @@ public class boardVisualization extends Application {
 
 
 
-    public void buildGrid(){
+    public GridPane buildGrid(){
+        routefinder.buildBoard(routefinder.readFile("/Users/olanordmann/Documents/Skole/5. Semester/AI/Øvinger/øving3/src/board-1-1.txt"));
         ArrayList<ArrayList<Node>> board = routefinder.getBoard();
 
         GridPane grid = new GridPane();
         grid.setGridLinesVisible(true);
 
+
         for (int i = 0; i < board.size(); i++) {
             ArrayList<Node> currentList = board.get(i);
+            grid.addRow(i);
+            System.out.println("Number of rows: "+i);
             for (int j = 0; j < currentList.size(); j++) {
-                Label currentLabel = new Label();
 
+                grid.addColumn(j);
+                Node currentNode = currentList.get(j);
+                Label currentLabel = createLabel(currentNode);
+                grid.add(currentLabel, currentNode.getX(), currentNode.getY());
+                System.out.println("CurrentNode coordinates: "+currentNode.getX()+","+currentNode.getY());
 
             }
 
-
-            grid.addRow();
         }
+        return grid;
 
     }
+
     public Label createLabel(Node node){
         Label newLabel = new Label();
 
         switch (node.getBoardchar()){
-            case '.': newLabel;
+            case '.': newLabel.setStyle("-fx-background-color: white");
+                break;
+            case '#': newLabel.setStyle("-fx-background-color: black");
+                break;
+            case 'A': newLabel.setStyle("-fx-background-color: red"); newLabel.setText("A"); newLabel.setTextAlignment(TextAlignment.CENTER);
+                break;
+            case 'B': newLabel.setStyle("-fx-background-color: turquoise"); newLabel.setText("B"); newLabel.setTextAlignment(TextAlignment.CENTER);
+                break;
+            case 'w': newLabel.setStyle("-fx-background-color: dodgerblue");
+                break;
+            case 'm': newLabel.setStyle("-fx-background-color: grey");
+                break;
+            case 'f': newLabel.setStyle("-fx-background-color: darkgreen");
+                break;
+            case 'g': newLabel.setStyle("-fx-background-color: lightgreen");
+                break;
+            case 'r': newLabel.setStyle("-fx-background-color: brown");
+                break;
         }
+        return newLabel;
     }
 
     public static void main(String[] args) {
@@ -48,6 +78,15 @@ public class boardVisualization extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        GridPane grid = buildGrid();
+        System.out.println(grid.toString());
+        Group root = new Group(grid);
+        Scene scene = new Scene(root);
+        grid.setAlignment(Pos.CENTER);
+
+        primaryStage.setTitle("Board Visualization.");
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
     }
 }
